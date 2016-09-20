a9fa78ebbf2e55b9664c84357ffe6e4915e6a6b5
HanSolo0001 <pillarite@gmail.com>
2016-09-20 03:05:08 -0400

    Added all the project files for different types of invaders and towers, interface for the invaders, level and game core files, exceptions, and a Random.cs file.

---
+++ TreehouseDefense/TreehouseDefense/SuperInvader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense
+{
+    class SuperInvader : Invader
+    {
+        public override int Health { get; protected set; } = 4;
+
+        protected override int StepSize { get; } = 2;
+
+        public SuperInvader(Path path) : base(path)
+        { }
+    }
+}
---
+++ TreehouseDefense/TreehouseDefense/SuperSniperTower.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense
+{
+    class SuperSniperTower : Tower
+    {
+        protected override int Range { get; } = 3;
+        protected override int Power { get; } = 2;
+        protected override double Accuracy { get; } = .90;
+
+        public SuperSniperTower(MapLocation location) : base(location)
+        { }
+    }
+}
